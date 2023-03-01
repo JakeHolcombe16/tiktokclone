34062c2478e2deaf030c47bde84e844d23f5b851
JakeHolcombe16 <jake.2021jatc@gmail.com>
2023-02-28 21:02:20 -0700

    first commit

---
+++ utils/constants.tsx
@@ -0,0 +1,34 @@
+import { BsCode, BsEmojiSunglasses } from 'react-icons/bs';
+import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
+import { FaPaw, FaMedal, FaGamepad, FaTv } from 'react-icons/fa';
+
+export const topics = [
+  {
+    name: 'comedy',
+    icon: <BsEmojiSunglasses />,
+  },
+  {
+    name: 'gaming',
+    icon: <FaGamepad />,
+  },
+  {
+    name: 'food',
+    icon: <GiCakeSlice />,
+  },
+  {
+    name: 'dance',
+    icon: <GiGalaxy />,
+  },
+  {
+    name: 'sports',
+    icon: <FaMedal />,
+  },
+  {
+    name: 'TV',
+    icon: <FaTv />,
+  },
+];
+
+export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carrers', 'ByteDance', 'Creator Directory']
+export const footerList2 = ['Targeted Advertisements','Developers','Stealing Your Data  👍','Rewards For Hours Spent' ]
+export const footerList3 = [ 'Terms of conditions', 'Privacy', 'Community Guidelines' ]
